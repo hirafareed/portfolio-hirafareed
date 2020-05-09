ee1985b00033fe50bb547cd3f09b3431109c657e
hirafareed <fareedh@sheridancollege.ca>
2020-05-09 14:25:19 -0400

    animation

--- src/components/WorkTogether.js
+++ src/components/WorkTogether.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "../sass/style.scss";
+import ScrollAnimation from "react-animate-on-scroll";
 
 const ContactPage = () => (
   <>
     <div className=" App layout-worktogether">
-      <h1 className="col-xl-6 col-lg-6 col-md-6 col-sm-8">
-        Let’s work together on your next project!
-      </h1>
-      <a href="mailto:hirafareedkapadia@gmail.com">
-        hirafareedkapadia@gmail.com
-      </a>
+      <ScrollAnimation animateIn="fadeIn" animateOut="fadeOut">
+        <h1 className="col-xl-6 col-lg-6 col-md-6 col-sm-8">
+          Let’s work together on your next project!
+        </h1>
+      </ScrollAnimation>
+      <ScrollAnimation animateIn="fadeIn" animateOut="fadeOut">
+        <a href="mailto:hirafareedkapadia@gmail.com">
+          hirafareedkapadia@gmail.com
+        </a>
+      </ScrollAnimation>
     </div>
   </>
 );

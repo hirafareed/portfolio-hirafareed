934d6f409ffcca4d4d1004e79d9b4e5db8072a5f
hirafareed <fareedh@sheridancollege.ca>
2020-05-09 14:11:21 -0400

    homepage-spacing

--- src/components/WorkTogether.js
+++ src/components/WorkTogether.js
@@ -4,7 +4,7 @@ import "../sass/style.scss";
 const ContactPage = () => (
   <>
     <div className=" App layout-worktogether">
-      <h1 className="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+      <h1 className="col-xl-6 col-lg-6 col-md-6 col-sm-8">
         Let’s work together on your next project!
       </h1>
       <a href="mailto:hirafareedkapadia@gmail.com">

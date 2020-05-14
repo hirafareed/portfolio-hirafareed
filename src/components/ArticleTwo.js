7b59a1da4216c9a33432fb71c353b658bfff57ed
hirafareed <fareedh@sheridancollege.ca>
2020-05-13 22:56:46 -0400

    fitted for mobile

--- src/components/ArticleTwo.js
+++ src/components/ArticleTwo.js
@@ -158,7 +158,7 @@ export default ({ match }) => {
               </p>
             </div>
           </div>
-          <div className="col-xl-6 col-lg-6 col-md-6 col-12 d-flex justify-content-center  ">
+          <div className="col-xl-6 col-lg-6 col-md-6 col-12 d-flex justify-content-center  image-fitted-formobile ">
             <video playsInline loop autoPlay muted className="wander-video">
               <source src={AnimationVideo} type="video/mp4" />
             </video>

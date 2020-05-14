e3569cc3825c78fbc88b79973239408ac304ac32
hirafareed <fareedh@sheridancollege.ca>
2020-05-14 01:15:09 -0400

    contact changes

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,20 +1,64 @@
 import React from "react";
 import "../sass/style.scss";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import ScrollAnimation from "react-animate-on-scroll";
 
 const ContactPage = () => (
   <>
-    <div className="App layout-contact ">
-      <div className="subheading large-container">
-        <hr className="line-contact" />
-        <h6 className="contact-sub ">Contact</h6>
-      </div>
-      <br></br>
+    <div className="App  ">
+      <div className="subheading large-container layout-contact">
+        <Row>
+          <Col>
+            {/* contact sub*/}
+            <div className="subheading">
+              <ScrollAnimation animateIn="fadeLeft">
+                <hr className="line-contact" />
+                <h6 className="contact-sub ">Contact</h6>
+              </ScrollAnimation>
+            </div>
+            <br></br>
+          </Col>
+        </Row>
 
-      {/* about-info */}
-      <div className="contact-info large-container">
-        <h1>Let's connect</h1>
+        {/* contact-info */}
+        <div className="about-info">
+          <Row>
+            <Col
+              lg={12}
+              xl={10}
+              md={10}
+              mdlg={3}
+              sm={10}
+              xs={10}
+              className="about-main "
+            >
+              <h1 className="text-primary">Let's Connect</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col
+              lg={{ offset: 3 }}
+              xl={{ offset: 3 }}
+              md={{ offset: 3 }}
+              className="about-body"
+            >
+              <a href="mailto:hirafareedkapadia@gmail.com">
+                hirafareedkapadia@gmail.com
+              </a>
+              <p>
+                If you would like to chat or have any questions, I would love to
+                hear from you at the email above. Feel free to connect with me
+                on social media!
+                <p className="contact-cheers">
+                  Cheers,<br></br>Hira
+                </p>
+              </p>
+            </Col>
+          </Row>
+        </div>
 
-        <a href="mailto:hirafareedkapadia@gmail.com">
+        {/* <a href="mailto:hirafareedkapadia@gmail.com">
           hirafareedkapadia@gmail.com
         </a>
 
@@ -25,7 +69,7 @@ const ContactPage = () => (
         </p>
         <p className="contact-cheers">
           Cheers,<br></br>Hira
-        </p>
+        </p> */}
       </div>
     </div>
   </>
